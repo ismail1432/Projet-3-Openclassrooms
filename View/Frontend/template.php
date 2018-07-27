74f764f880cca2a38e0a42fea4d6e00c9a8712f4
Zalati <y4su.ced@gmail.com>
2018-07-27 16:48:00 +0200

    Pushing Frontend View

---
+++ View/Frontend/template.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8"> <!-- Balise meta -->
+        <meta name="viewport" content="initial-scale=1, user-scalable=no">
+        <link rel="icon" type="image/png" href="Contenu/images/favicon.png">
+        <link href="https://fonts.googleapis.com/css?family=Pattaya" rel="stylesheet"> <!-- Police d'écriture "Pattaya" -->
+        <link href="https://fonts.googleapis.com/css?family=El+Messiri" rel="stylesheet"> <!-- Police d'écriture "El Messiri" -->
+        <link rel="stylesheet" href="contenu/font-awesome/css/font-awesome.min.css"> <!-- Font Awesome -->
+        <link rel="stylesheet" href="assets/css/style.css"> <!-- Feuille de style général -->
+        <link rel="stylesheet" href="assets/css/styleFrontend.css"> <!-- Feuille de style du Frontend -->
+        <!-- Titre du site définit par la variable $titrePage -->
+        <title><?= $this->title(); ?></title>
+    </head>
+
+    <body>
+
+        <header>
+            <p><img src="assets/images/logo.png"></p>
+            <h1>Billet simple pour l'Alaska</h1>
+        </header>
+
+        <nav>
+            <ul id="oneLevel">
+                <li><span class="fa fa-home"></span><a href="index.php" title="Retourner à l'accueil du blog">Accueil</a></li>
+                <ul id="secondLevel">
+                <?php if(isset($_SESSION['emailAdress']))
+                {
+                    if($_SESSION['statusMember'] == 'administrateur')
+                    {
+                    ?>
+                        <li><a href="admin.php" title="Accéder à l'espace administration du blog">Administrer le Blog</a></li> <!-- Lien vers l'administration du site -->
+                    <?php
+                    }
+                    ?>
+                        <li><a href="index.php?action=deconnect" title="Se déconnecter">Deconnexion</a></li> <!-- Lien de deconnexion -->
+                    <?php
+                }
+                else
+                {
+                ?>
+                    <li><a href="index.php?action=connection" title="Connecter vous">Se connecter</a></li> <!-- Lien de connexion -->
+                    <li><a href="index.php?action=registration" title="Créer un compte">Créer un compte</a></li> <!-- Lien vers la création d'un compte -->
+                <?php
+                }
+                ?>
+                </ul>
+            </ul>
+        </nav>
+
+        <!-- Affichage du flux -->
+        <?= $content; ?>
+
+        <script src="assets/js/tinymce/tinymce.min.js"></script> <!-- Fichier JS de tinymce -->
+        <script src="assets/js/tinymceIndex.js"></script> <!-- Fichier initialisation tinymce -->
+
+    </body>
+</html>
---
+++ View/Frontend/viewConnect.php
@@ -0,0 +1,32 @@
+<?php
+if(isset($_SESSION['idMember']))
+{
+    // Redirige le membre vers index.php
+    header('Location:index.php');
+}
+
+$this->setTitle('Page de connexion - Billet simple pour l\'Alaska'); ?>
+
+<section id="formConnectionRegistration">
+
+    <form method="post" action="index.php?action=connection">
+
+        <?php if(isset($_SESSION['errorConnect'])) { echo '<p class="errorBlog">' . $_SESSION['errorConnect'] . '</p>'; } ?>
+
+        <p>
+            <label for="emailAdress">Votre adresse E-Mail :</label><br>
+            <input type="email" name="emailAdress" id="emailAdress">
+        </p>
+
+        <p>
+            <label for="password">Votre mot de passe :</label><br>
+            <input type="password" name="password" id="password">
+        </p>
+
+        <p>
+            <input type="submit" class="buttonBlog" value="connexion">
+        </p>
+
+    </form>
+
+</section>
