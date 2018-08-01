69cc59dba0c941ec23808c0c84fac2d046729b6a
Zalati <y4su.ced@gmail.com>
2018-08-01 03:05:48 +0200

    Pushing backend view with PSR-2 fix

---
+++ View/backend/template.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8"> <!-- Balise meta -->
+        <meta name="viewport" content="initial-scale=1, user-scalable=no">
+        <link rel="icon" type="image/png" href="assets/images/favicon.png">
+        <link href="https://fonts.googleapis.com/css?family=Pattaya" rel="stylesheet"> <!-- Police d'écriture "Pattaya" -->
+        <link href="https://fonts.googleapis.com/css?family=El+Messiri" rel="stylesheet"> <!-- Police d'écriture "El Messiri" -->
+        <link rel="stylesheet" href="assets/css/style.css"> <!-- Feuille de style Géneral -->
+        <link rel="stylesheet" href="assets/css/styleBackend.css"> <!-- Feuille de style spécifique au Backend -->
+        <!-- Titre du site définit par la variable $titrePage -->
+        <title><?= $this->title(); ?></title>
+    </head>
+
+    <body>
+
+        <header>
+            <p><img src="assets/images/logo.png"></p>
+            <h1>Billet simple pour l'Alaska</h1>
+        </header>
+
+        <nav>
+            <ul id="firstLevel">
+                <li><a href="index.php" title="Retourner sur le blog">Visualiser le Blog</a></li>
+                <ul id="secondLevel">
+                <?php if (isset($_SESSION['emailAdress'])) {
+    ?>
+                    <li><a href="index.php?action=deconnect" title="Me deconnecter du blog">Deconnexion</a></li> <!-- Lien de deconnexion -->
+                </ul>
+                <?php
+}
+                ?>
+                </ul>
+            </ul>
+        </nav>
+
+        <!-- Affichage du flux -->
+        <?= $content; ?>
+
+        <script src="assets/js/tinymce/tinymce.min.js"></script> <!-- Fichier JS de tinymce -->
+        <script src="assets/js/tinymceAdmin.js"></script> <!-- Fichier initialisation tinymce -->
+
+    </body>
+</html>
---
+++ View/backend/viewComment.php
@@ -0,0 +1,59 @@
+<!--***************************************************
+* Cette page affichera les commentaires d'un chapitre *
+****************************************************-->
+<?php
+// Titre de la page
+if (isset($comments[0])) {
+    $this->setTitle('Commentaires : ' . $comments[0]->titleTicket() . ' - Billet simple pour l\'Alaska');
+} else {
+    $this->setTitle('Aucun commentaires pour ce chapitre - Billet simple pour l\'Alaska');
+}
+?>
+
+<span class="linkPage"><a href="admin.php">Voir la liste des chapitres</a></span> <!-- Lien vers la liste des chapitres -->
+
+<!-- Affichage des commentaires -->
+<section id="comments">
+    <h1><?= $titleSection; ?></h1>
+
+    <?php
+    if (count($comments) != 0) {
+        for ($i = 0; $i < count($comments); $i++) :
+
+        if (isset($_GET['idTicket'])) {
+            $idTicket = $_GET['idTicket'];
+        } else {
+            $idTicket = $comments[$i]->idTicket();
+        } ?>
+        <article <?php if ($comments[$i]->alertComment()) {
+            ?>class="commentAlert"<?php
+        } else {
+            ?>class="comment"<?php
+        } ?>>
+            <div class="headerCommentaire">
+                <p>Poster par : <span><?= $comments[$i]->mailMember(); ?></span></p>
+                <p>Poster le : <span><?= $comments[$i]->dateTimeAddComment(); ?></span></p>
+            </div>
+
+            <div class="contentComment">
+                <?= $comments[$i]->contentComment(); ?>
+            </div>
+
+            <ul>
+                <li><a href="admin.php?action=delete&deleteComment=on&idComment=<?= $comments[$i]->idComment(); ?>" title="Supprimer le commentaire">Supprimer</a></li>
+                <li><a href="index.php?action=ticket&id=<?= $idTicket; ?>" title="Visualiser le chapitre du commentaire" target=_blank>Visualiser le chapitre</a></li>
+            <?php
+            if ($comments[$i]->alertComment()) {
+                ?>
+                <li><a href="admin.php?action=comment&approve=on&id=<?= $comments[$i]->idComment(); ?>" title="Approuver le commentaire signaler">Approuver le commentaire</a></li>
+            <?php
+            } ?>
+            </ul>
+        </article>
+    <?php
+        endfor;
+    } else {
+        echo 'Aucun commentaire n\'a encore était poster pour ce chapitre !';
+    }
+    ?>
+</section> <!-- /Affichage des commentaires -->
