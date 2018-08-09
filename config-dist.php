2aa5703d4f4bdf11b3468fba44b4f660c0468188
Zalati <y4su.ced@gmail.com>
2018-08-09 23:51:36 +0200

    Add files via upload

---
+++ config-dist.php
@@ -0,0 +1,7 @@
+<?php
+    // identifiants connexion WAMP
+    // Constante de connexion
+    define("HOST_NAME","HostDeLaDB");
+    define("DATABASE","NomDeVotreBaseDeDonnee");
+    define("USER_NAME","VotreNomDutilisateur");
+    define("PASSWORD", "VotreMotDePasse");
